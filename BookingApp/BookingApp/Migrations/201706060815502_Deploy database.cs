5b0e45ef98409e972ae79de20474deb4d489860a
AleksandraMisic <alexmisic94@gmail.com>
2017-06-06 10:30:32 +0200

    Dodati seteri

---
+++ BookingApp/BookingApp/Migrations/201706060815502_Deploy database.cs
@@ -0,0 +1,16 @@
+namespace BookingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Deploydatabase : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- BookingApp/BookingApp/Models/Context/BAContext.cs
+++ BookingApp/BookingApp/Models/Context/BAContext.cs
@@ -11,10 +11,14 @@ namespace BookingApp.Models
     public class BAContext: IdentityDbContext<BAIdentityUser>
     {   
         public virtual DbSet<AppUser> AppUsers { get; set; }
-
-        public BAContext() : base("name=JD2_TastaturaDB")
-        {            
-        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomReservations> RoomReservationss { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Accommodation> Accommodations { get; set; }
+        public DbSet<AccommodationType> AccommodationTypes { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Country> Countries { get; set; }
 
         public static BAContext Create()
         {
