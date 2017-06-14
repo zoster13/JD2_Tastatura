8ea5c5c59974681ab5b8cf0bf3e71eabe680e2af
AleksandraMisic <alexmisic94@gmail.com>
2017-06-14 10:11:58 +0200

    Add accommodation forma, radi i room

--- BookingApp/AngularUI/Angular/src/app/add-accommodation/add-accommodation.component.ts
+++ BookingApp/AngularUI/Angular/src/app/add-accommodation/add-accommodation.component.ts
@@ -9,6 +9,7 @@ import {NgForm} from '@angular/forms';
 import {PlacesService} from '../services/places.service';
 import {AccommodationTypesService} from '../services/accommodation-types.service';
 import {AccommodationService} from '../services/accommodation.service';
+import {RoomsService} from '../services/rooms.service';
 
 @Component({
   selector: 'add-accomm',
@@ -23,10 +24,12 @@ export class AddAccommodationComponent implements OnInit {
   users: AppUser [];
   rooms: Room [];
   accommodation: Accommodation;
+  place: number;
 
   constructor(private placesService:PlacesService,
   private accommTypeService: AccommodationTypesService,
-  private accomService: AccommodationService
+  private accomService: AccommodationService,
+  private roomsService: RoomsService
   ) 
   {
     this.rooms = [];
@@ -49,13 +52,27 @@ export class AddAccommodationComponent implements OnInit {
       .then(accommTypes => this.accommTypes = accommTypes);
   }
 
-  onSubmitAccomm(accomm: Accommodation, form: NgForm) {
-      this.accomService.create(accomm);
+  onSubmitAccomm(accomm: any, form: NgForm) {
+      this.accommodation.name = accomm.Name;
+      this.accommodation.description = accomm.Description;  
+      this.accommodation.address = accomm.Address;
+      this.accommodation.longitude = accomm.Longitude;
+      this.accommodation.latitude = accomm.Latitude;
+      this.accommodation.place = new Place();
+      this.accommodation.place.id = accomm.Place;
+      this.accommodation.accommodationType = new AccommodationType();
+      this.accommodation.accommodationType.id = accomm.AccommodationType;
+      this.accommodation.rooms = this.rooms;
+
+      debugger
+      this.accomService.create(this.accommodation);
+      
     }
 
     onSubmitRoom(room: Room, form: NgForm) {
       this.rooms.push(room);
       this.accommodation.rooms.push(room);
+      debugger
       form.reset();
     }
 }
