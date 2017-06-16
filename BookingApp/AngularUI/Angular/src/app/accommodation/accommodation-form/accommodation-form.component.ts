490102e0b7db32a1458e9f75da50553c7090b46d
AleksandraMisic <alexmisic94@gmail.com>
2017-06-16 11:04:39 +0200

    Ispravka formi, prikaz slike

--- BookingApp/AngularUI/Angular/src/app/accommodation/accommodation-form/accommodation-form.component.ts
+++ BookingApp/AngularUI/Angular/src/app/accommodation/accommodation-form/accommodation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef   } from '@angular/core';
 import {Router} from '@angular/router';
 import { Place } from '../../models/Place';
 import { Room } from '../../models/Room';
@@ -21,6 +21,9 @@ import {AppUsersService} from '../../services/appuser.service';
 
 export class AccommodationFormComponent implements OnInit {
 
+  fileSrcs: string[] = [];  
+  image: any;
+
   places: Place[];
   accommTypes: AccommodationType [];
   users: AppUser [];
@@ -37,7 +40,8 @@ export class AccommodationFormComponent implements OnInit {
   private accomService: AccommodationService,
   private roomsService: RoomsService,
   private appUsersService: AppUsersService,
-  private router: Router
+  private router: Router,
+  private changeDetectorRef: ChangeDetectorRef
   ) 
   {
     this.accommodation = new Accommodation();
@@ -68,6 +72,9 @@ export class AccommodationFormComponent implements OnInit {
       this.accommodation.address = this.temp.address;
       this.accommodation.longitude = this.temp.longitude;
       this.accommodation.latitude = this.temp.latitude;
+      this.accommodation.imageURL = this.temp.imageURL;
+      this.fileSrcs = [];
+      this.fileSrcs.push(this.accommodation.imageURL);
     }
     else{
       this.isUpdate = false;
@@ -77,6 +84,7 @@ export class AccommodationFormComponent implements OnInit {
       this.accommodation.address = '';
       this.accommodation.longitude = 0;
       this.accommodation.latitude = 0;
+      this.fileSrcs = [];
     }
   }
 
@@ -117,5 +125,79 @@ export class AccommodationFormComponent implements OnInit {
       form.resetForm();
 
       this.router.navigate(["mainpage/accommodation/accommlist"]);
+    }
+
+    fileChange(input) {  
+      this.accommodation.imageURL = input.files[0].name;
+      this.readFiles(input.files);  
+    } 
+
+    readFile(file, reader, callback) {  
+        reader.onload = () => {  
+            callback(reader.result);  
+            this.image = reader.result;
+            console.log(reader.result);  
+        }  
+        reader.readAsDataURL(file);  
+    }  
+    readFiles(files, index = 0) {  
+       let reader = new FileReader();  
+        // If there is a file  
+        if (index in files) {  
+            // Start reading this file  
+            this.readFile(files[index], reader, (result) => {  
+                // Create an img element and add the image file data to it  
+                var img = document.createElement("img");  
+                img.src = result;  
+                // Send this img to the resize function (and wait for callback)  
+                this.resize(img, 250, 250, (resized_jpeg, before, after) => {  
+                    // For debugging (size in bytes before and after)  
+                    //this.debug_size_before.push(before);  
+                    //this.debug_size_after.push(after);  
+                    // Add the resized jpeg img source to a list for preview  
+                    // This is also the file you want to upload. (either as a  
+                    // base64 string or img.src = resized_jpeg if you prefer a file).  
+                    this.fileSrcs = [];
+                    this.fileSrcs.push(resized_jpeg);  
+                    // Read the next file;  
+                    this.readFiles(files, index + 1);  
+                });  
+            });  
+        } else {  
+            // When all files are done This forces a change detection 
+            this.changeDetectorRef.detectChanges();  
+        }  
+    }  
+    resize(img, MAX_WIDTH: number, MAX_HEIGHT: number, callback) {  
+        // This will wait until the img is loaded before calling this function  
+        return img.onload = () => {  
+            // Get the images current width and height  
+            var width = img.width;  
+            var height = img.height;  
+            // Set the WxH to fit the Max values (but maintain proportions)  
+            if (width > height) {  
+                if (width > MAX_WIDTH) {  
+                    height *= MAX_WIDTH / width;  
+                    width = MAX_WIDTH;  
+                }  
+            } else {  
+                if (height > MAX_HEIGHT) {  
+                    width *= MAX_HEIGHT / height;  
+                    height = MAX_HEIGHT;  
+                }  
+            }  
+            // create a canvas object  
+            var canvas = document.createElement("canvas");  
+            // Set the canvas to the new calculated dimensions  
+            canvas.width = width;  
+            canvas.height = height;  
+            var ctx = canvas.getContext("2d");  
+            ctx.drawImage(img, 0, 0, width, height);  
+            // Get this encoded as a jpeg  
+            // IMPORTANT: 'jpeg' NOT 'jpg'  
+            var dataUrl = canvas.toDataURL('image/jpeg');  
+            // callback with the results  
+            callback(dataUrl, img.src.length, dataUrl.length);  
+        };  
     }
 }
