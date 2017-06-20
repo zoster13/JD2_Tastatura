582988b4144e56a788870e0ef23f81efa1000db7
AleksandraMisic <alexmisic94@gmail.com>
2017-06-20 16:14:23 +0200

    Rooms tab dozvoljen menadzeru

--- BookingApp/AngularUI/Angular/src/app/rooms/rooms-main/rooms-main.component.ts
+++ BookingApp/AngularUI/Angular/src/app/rooms/rooms-main/rooms-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'rooms',
@@ -8,8 +9,24 @@ import { Router } from '@angular/router';
 })
 
 export class RoomsMainComponent {
+  role: string;
+  banned: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+  private authenticationService: AuthenticationService) {
     
   }
+
+  isLoggedIn(): boolean {
+        return this.authenticationService.isLoggedIn();
+  }
+
+  ngOnInit() : void {
+        if(this.isLoggedIn()){
+            this.role = JSON.parse(localStorage.getItem('currentUser'))['role'];
+            if(this.role == 'Manager'){
+                this.banned = JSON.parse(localStorage.getItem('currentUser'))['isBanned'];
+            }
+        }
+    }
 }
